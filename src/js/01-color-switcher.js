de8086ffa01644c3b4e231fce2fa0744177b2256
Artemskyba <artemgennadievich@icloud.com>
2023-09-13 15:37:26 +0300

    first_task_done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,51 @@
+bodyEl = document.querySelector('body');
+startBtnEl = document.querySelector('[data-start]');
+stopBtnEl = document.querySelector('[data-stop]');
 
+let colorParty;
+
+const wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
+wrapper.append(startBtnEl, stopBtnEl);
+bodyEl.append(wrapper);
+
+bodyEl.style.width = '100vh';
+bodyEl.style.height = '100vh';
+
+wrapper.style.position = 'absolute';
+wrapper.style.top = '50%';
+wrapper.style.left = '50%';
+wrapper.style.transform = 'translate(-50%, -50%)';
+wrapper.style.display = 'flex';
+wrapper.style.gap = '20px';
+
+stopBtnEl.disabled = true;
+stopBtnEl.style.width = '110px';
+stopBtnEl.style.height = '40px';
+startBtnEl.style.width = '110px';
+startBtnEl.style.height = '40px';
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+function onStartBtnClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+
+  colorParty = setInterval(changeBodyBg, 1000);
+
+  function changeBodyBg() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }
+};
+
+function onStopBtnClick() {
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+  clearInterval(colorParty);
+};
