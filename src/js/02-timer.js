3c134c42045d8cdd3db417208be07afbc0f19d66
Artemskyba <artemgennadievich@icloud.com>
2023-09-15 14:32:44 +0300

    corrected_second_task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,20 +2,12 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-const timerEl = document.querySelector('.timer');
-const bodyEl = document.querySelector('body');
 const startBtnEl = document.querySelector('[data-start]');
 const inputDateEl = document.querySelector('#datetime-picker');
 const daysCounterEl = document.querySelector('[data-days]');
 const hoursCounterEl = document.querySelector('[data-hours]');
 const minutesCounterEl = document.querySelector('[data-minutes]');
 const secondsCounterEl = document.querySelector('[data-seconds]');
-
-const wrapper = document.createElement('div');
-wrapper.classList.add('wrapper');
-wrapper.append(inputDateEl, startBtnEl, timerEl);
-bodyEl.append(wrapper);
-
 
 startBtnEl.disabled = true;
 
@@ -54,30 +46,26 @@ function convertMs(ms) {
 startBtnEl.addEventListener('click', onStartBtnClick);
 
 function onStartBtnClick() {
-  let timerDateMs = new Date(inputDateEl.value).getTime() - new Date().getTime();
-    const timerDateObj = convertMs(timerDateMs);
-    setTimer(timerDateObj);
-
+  setTimer()
   const intervalTimer = setInterval(() => { 
-    const timerDateObj = convertMs(timerDateMs);
-    setTimer(timerDateObj);
-    console.log(timerDateObj);
-    timerDateMs -= 1000;
-
-    if (timerDateMs < 0) {
-      startBtnEl.disabled = true;
-      clearInterval(intervalTimer);
-    }
-  }, 1000);
+    setTimer(1000)}, 1000);
 };
 
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
-}
+};
 
-function setTimer ({ days, hours, minutes, seconds }) {
+function setTimer(delay) {
+  let timerDateMs = new Date(inputDateEl.value).getTime() - new Date().getTime();
+  const {days, hours, minutes, seconds} = convertMs(timerDateMs);
   daysCounterEl.textContent = addLeadingZero(days);
   hoursCounterEl.textContent = addLeadingZero(hours);
   minutesCounterEl.textContent = addLeadingZero(minutes);
   secondsCounterEl.textContent = addLeadingZero(seconds);
+  timerDateMs -= delay;
+
+  if (timerDateMs < 0) {
+  startBtnEl.disabled = true;
+  clearInterval(intervalTimer);
+  };
 };
